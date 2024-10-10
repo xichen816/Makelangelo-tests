6425cb045e717faf635383be3f70e70219f39571
xichen816 <128641225+xichen816@users.noreply.github.com>
2024-10-09 23:04:10 -0400

    changed tests
    added 3 tests for LineInterpolatorSinCurve, ColorHSB, StringHelper and RangeSlider

--- src/test/java/com/marginallyclever/convenience/TestColorHSB.java
+++ src/test/java/com/marginallyclever/convenience/TestColorHSB.java
@@ -1,7 +1,8 @@
 package com.marginallyclever.convenience;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 
 public class TestColorHSB {
     private void assertColorsEqual(ColorHSB c,int hue,int saturation,int brightness) {
@@ -22,5 +23,36 @@ public class TestColorHSB {
         assertColorsEqual(new ColorHSB(2,0,0),1,0,0);
         assertColorsEqual(new ColorHSB(0,2,0),0,1,0);
         assertColorsEqual(new ColorHSB(0,0,2),0,0,1);
+    }
+
+    @Test
+    public void testEquals() {
+        // Object is equal to itself
+        ColorHSB color = new ColorHSB(-2, 4, 6);
+        Assertions.assertTrue(color.equals(color));
+
+        // Two equal objects
+        ColorHSB color1 = new ColorHSB(1, 2, 1);
+        ColorHSB color2 = new ColorHSB(1, 2, 1);
+        Assertions.assertTrue(color1.equals(color2));
+
+        // Different hue
+        ColorHSB colorDifferentHue = new ColorHSB(0, 2, 1);
+        Assertions.assertFalse(color1.equals(colorDifferentHue));
+
+        // Different saturation
+        ColorHSB colorDifferentSaturation = new ColorHSB(1, -1, 1);
+        Assertions.assertFalse(color1.equals(colorDifferentSaturation));
+
+        // Different brightness
+        ColorHSB colorDifferentBrightness = new ColorHSB(1, 2, 0);
+        Assertions.assertFalse(color1.equals(colorDifferentBrightness));
+
+        // Null comparison
+        Assertions.assertFalse(color1.equals(null));
+
+        // Different class comparison
+        Object differentClassObject = new Object();
+        Assertions.assertFalse(color1.equals(differentClassObject));
     }
 }
---
+++ src/test/java/com/marginallyclever/makelangelo/TestRangeSlider.java
@@ -0,0 +1,45 @@
+package com.marginallyclever.makelangelo;
+
+import com.marginallyclever.makelangelo.rangeslider.RangeSlider;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestRangeSlider {
+    @Test
+    public void testSetValue() {
+
+        // setting a value
+        RangeSlider rangeSlider = new RangeSlider(0, 10);
+        rangeSlider.setValue(5);
+        assertEquals(5, rangeSlider.getValue());
+
+        // setting a lower value
+        rangeSlider.setValue(4);
+        assertEquals(4, rangeSlider.getValue());
+        assertEquals(1, rangeSlider.getExtent());
+
+        // setting  a higher value
+        rangeSlider.setValue(3);
+        rangeSlider.setValue(4);
+        assertEquals(4, rangeSlider.getValue());
+        assertEquals(1, rangeSlider.getExtent());
+
+        // setting the minimum value
+        rangeSlider.setValue(0);
+        assertEquals(0, rangeSlider.getValue());
+
+        // set the maximum value
+        rangeSlider.setUpperValue(10);
+        rangeSlider.setValue(9);
+        assertEquals(9, rangeSlider.getValue());
+        assertEquals(1, rangeSlider.getExtent());
+
+        // try to set below minimum
+        rangeSlider.setValue(-1);
+        assertEquals(0, rangeSlider.getValue(), "should not go below minimum");
+
+        // try to set above maximum
+        rangeSlider.setValue(11);
+        assertEquals(10, rangeSlider.getValue(), "should not go above maximum");
+    }
+}
